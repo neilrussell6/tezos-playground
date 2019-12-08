fc582aeaa2cf4e76bd7a2940e3e30046ac72d2b4
Neil Russell <neilrussell6@gmail.com>
2019-12-08 21:35:16 +0200

    refactor(doubler): cleanup doubler smart contract.

--- src/contracts/doubler/doubler.py
+++ src/contracts/doubler/doubler.py
@@ -4,10 +4,8 @@ import vendor.SmartPyBasic.smartpy as sp
 
 class DoublerContract(sp.Contract):
     def __init__(self):
-        print("--------- DoublerContract :: init")
         self.init(storage=0)
 
     @sp.entryPoint
     def double(self, value):
-        print("--------- DoublerContract :: double")
         self.data.storage = value + value
